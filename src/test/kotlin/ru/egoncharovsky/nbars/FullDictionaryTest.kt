ceff19a16822722aba190ec04eb3ffde7f7988d2
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2021-03-31 01:40:09 +0300

    parallel reading in test and collecting statistic

--- src/test/kotlin/ru/egoncharovsky/nbars/FullDictionaryTest.kt
+++ src/test/kotlin/ru/egoncharovsky/nbars/FullDictionaryTest.kt
@@ -1,14 +1,16 @@
 package ru.egoncharovsky.nbars
 
+import kotlinx.coroutines.*
 import mu.KotlinLogging
 import org.junit.jupiter.api.Test
+import ru.egoncharovsky.nbars.entity.Article
 import ru.egoncharovsky.nbars.parse.ArticleParser
 import java.io.File
-import java.lang.Exception
+import kotlin.system.measureTimeMillis
 
 internal class FullDictionaryTest {
 
-    private val logger = KotlinLogging.logger {  }
+    private val logger = KotlinLogging.logger { }
 
     private val dictionaryFile = File("src/main/resources/dictionary.dsl")
     private val indexFile = File("index/full_test.dsl.index")
@@ -18,20 +20,47 @@ internal class FullDictionaryTest {
         val reader = DictionaryReader(dictionaryFile, indexFile)
         val parser = ArticleParser()
         val positions = reader.readArticlePositions()
-        var i=1
-        var errors=0
+        val headwords = positions.keys.toList()
 
-        positions.forEach { (headword, position) ->
-            val lines = reader.readArticle(position)
-            try {
-                parser.parse(headword, lines)
-                i++
-            } catch (e: Exception) {
-                logger.error("Exception during parse '$headword' ($i from ${positions.size}): ${e.message}")
-                errors++
-//                throw Exception("Exception during parse '$headword' ($i from ${positions.size})", e)
+        val results: List<Pair<String, Result<Article>>>
+        val time = measureTimeMillis {
+            results = runBlocking {
+                withContext(Dispatchers.IO) {
+                    positions.map { (headword, position) ->
+                        async {
+                            val lines = reader.readArticle(position)
+                            headword to kotlin.runCatching {
+                                parser.parse(headword, lines)
+                            }
+                        }
+                    }.awaitAll()
+                }
             }
         }
-        logger.info("Total errors: $errors (${errors*100 / positions.size} %)")
+
+        val exceptionLines = mutableMapOf<StackTraceElement, Int>()
+        val exceptionMessages = mutableMapOf<StackTraceElement, String>()
+
+
+        results.forEach { (headword, result) ->
+            result.exceptionOrNull()?.let {
+                val element =
+                    it.stackTrace.find { it.className == "ru.egoncharovsky.nbars.parse.ArticleParser" }!!
+                exceptionLines.putIfAbsent(element, 0)
+                exceptionMessages.putIfAbsent(element, "for '$headword' (#${headwords.indexOf(headword)}): ${it.message!!}")
+                exceptionLines[element] = exceptionLines[element]!! + 1
+            }
+        }
+        logger.info("Finished in ${seconds(time)}")
+
+        val errors = exceptionLines.values.sum()
+        val sorted = exceptionLines.toList()
+            .sortedByDescending { (_, value) -> value }
+            .joinToString("\n") { (element, count) -> "$count\t\tat $element, ${exceptionMessages[element]}" }
+
+        logger.error("Exceptions occurred at\n$sorted")
+        logger.error("Total errors: $errors (${errors * 100 / positions.size} %)")
     }
+
+    private fun seconds(millis: Long) = "${millis / 1000}.${millis % 1000} s"
 }
