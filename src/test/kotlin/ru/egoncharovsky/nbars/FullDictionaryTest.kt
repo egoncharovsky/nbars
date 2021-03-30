83894103cce26453d5908b59287ea5afdceae09f
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2021-03-30 23:09:53 +0300

    full test wide analyze

--- src/test/kotlin/ru/egoncharovsky/nbars/FullDictionaryTest.kt
+++ src/test/kotlin/ru/egoncharovsky/nbars/FullDictionaryTest.kt
@@ -1,5 +1,6 @@
 package ru.egoncharovsky.nbars
 
+import mu.KotlinLogging
 import org.junit.jupiter.api.Test
 import ru.egoncharovsky.nbars.parse.ArticleParser
 import java.io.File
@@ -7,15 +8,18 @@ import java.lang.Exception
 
 internal class FullDictionaryTest {
 
+    private val logger = KotlinLogging.logger {  }
+
     private val dictionaryFile = File("src/main/resources/dictionary.dsl")
     private val indexFile = File("index/full_test.dsl.index")
 
     @Test
-    internal fun fullDictionary() {
+    fun fullDictionary() {
         val reader = DictionaryReader(dictionaryFile, indexFile)
         val parser = ArticleParser()
         val positions = reader.readArticlePositions()
         var i=1
+        var errors=0
 
         positions.forEach { (headword, position) ->
             val lines = reader.readArticle(position)
@@ -23,8 +27,11 @@ internal class FullDictionaryTest {
                 parser.parse(headword, lines)
                 i++
             } catch (e: Exception) {
-                throw Exception("Exception during parse '$headword' ($i from ${positions.size})", e)
+                logger.error("Exception during parse '$headword' ($i from ${positions.size}): ${e.message}")
+                errors++
+//                throw Exception("Exception during parse '$headword' ($i from ${positions.size})", e)
             }
         }
+        logger.info("Total errors: $errors (${errors*100 / positions.size} %)")
     }
 }
